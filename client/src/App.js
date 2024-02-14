c879cea2848bfbf70673a9ba389a93b554a8a542
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-14 11:55:37 +0530

    input container made

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Register from "./pages/Register.js"; 
-import Login from "./pages/Login.js"; 
-import Chat from "./pages/Chat.js"; 
-import SetAvatar from "./pages/SetAvatar.js"
+// import Register from "./pages/Register.js"; 
+// import Login from "./pages/Login.js"; 
+// import Chat from "./pages/Chat.js"; 
+// import SetAvatar from "./pages/SetAvatar.js"
+import ChatInput from './components/ChatInput.js';
 // import Contacts from "./components/Contacts.js"
 // import Welcome from "./components/Welcome.js"
 
@@ -12,11 +13,10 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Contacts />} /> */}
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/setAvatar" element={<SetAvatar />} />
-          <Route path="/chat" element={<Chat />} />
+          {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+          <Route path="/" element={<ChatInput />} />
         </Routes>
       </BrowserRouter>
     </div>
